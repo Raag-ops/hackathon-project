a2b643c3f75cb587d8815a6ce7c52902afac04f7
Samridha <121121528+Nitride27@users.noreply.github.com>
2024-11-22 09:48:51 +0545

    Add files via upload

--- lib/services/auth_services.dart
+++ lib/services/auth_services.dart
@@ -2,31 +2,21 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class AuthService {
-  // Method to authenticate user with email and password
-  static Future<String> authenticate(String email, String password) async {
+  static Future<String> loginUser(String email, String password) async {
     try {
-      // Attempt to sign in with provided email and password
       UserCredential userCredential = await FirebaseAuth.instance
           .signInWithEmailAndPassword(email: email, password: password);
 
-      // Fetch user data from Firestore
+      // Fetch user role from Firestore
       DocumentSnapshot userDoc = await FirebaseFirestore.instance
           .collection('users')
           .doc(userCredential.user!.uid)
           .get();
 
-      // Check if the user document exists and contains the role field
-      if (userDoc.exists && userDoc.data() != null && userDoc['role'] != null) {
-        return userDoc['role']; // Return the role (admin, user, etc.)
-      } else {
-        // If user document doesn't exist or role is missing, return error
-        return 'role_not_found';
-      }
+      return userDoc['role'];
     } catch (e) {
-      // Print the error and return a general error message
-      print("Authenticate Error: $e");
+      print("Login Error: $e");
       return 'error';
     }
   }
 }
-
